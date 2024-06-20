5068da9515d0fb7baecab3465d3fb585d7084df5
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-06-20 16:26:28 -0300

    transform project in .tsx and tree ok

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Contato = () => {
+  return (
+    <>
+      <head>
+        <title>Contato | Kdev</title>
+      </head>
+      <main>
+        <h1>Sinta se a vontade para entrar em contato conosco</h1>
+        <Link href="instagram.com/kauannmoreira_"></Link>
+        <Link href="/">Voltar para a página inicial</Link>
+      </main>
+    </>
+  );
+}
+
+export default Contato;
